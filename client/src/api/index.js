4ff0f925ecdce6987baeeaf3f016d0d411ff7c82
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-02-09 14:11:15 +0200

    front refresh done

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,13 +3,28 @@ const httpClient = axios.create({
     baseURL: 'http://localhost:5000/api'
 });
 
+
+
+/* Auth api */
+
+export const signIn = async (userData) => await httpClient.post('/users/sign-in', userData);
+
+export const signUp = async (userData) => await httpClient.post('/users/sign-up', userData);
+
 export const logOut = async () => {
     localStorage.clear();
 }  
 
 
+export const refreshSession = async () => {
+    const refreshToken = localStorage.getItem('refreshToken');
+    const {data} = await httpClient.post('/users/refresh', {refreshToken});
+    return data;
+}
+
+
 httpClient.interceptors.request.use((config) => {
-    const token = localStorage.getItem('token');
+    const token = localStorage.getItem('accessToken');
     if(token) {
         config.headers = {
             ...config.headers,
@@ -18,26 +33,34 @@ httpClient.interceptors.request.use((config) => {
     }
     return config;
 }, (err) => Promise.reject(err));
+
 httpClient.interceptors.response.use((response) => {
-    if(response.data.token) {
-        const {data: {token}} = response;
-        localStorage.setItem('token', token);
+    if(response.data.tokens) {
+        const {data: {tokens: {accessToken, refreshToken}}} = response;
+        localStorage.setItem('accessToken', accessToken);
+        localStorage.setItem('refreshToken', refreshToken);
     }
     return response;
 }, (err) => {
-    if (err.response.status === 403) {
+    if (err.response.status === 403 && localStorage.getItem('refreshToken')) {
+       console.log('REFRESH');
+
+       return refreshSession()
+       .then(() => {
+        console.log('RETRY request');
+        return httpClient(err.config);
+       })
+
+    } else if (err.response.status === 401) {
         logOut();
+        /// HERE - redirect to home
+    } else {
+        return Promise.reject(err);
     }
-    return Promise.reject(err);
 })
 
 
 
-
-/* Auth api */
-
-export const signIn = async (userData) => await httpClient.post('/users/sign-in', userData);
-export const signUp = async (userData) => await httpClient.post('/users/sign-up', userData);
 /* Chat api  
 */
 export const createChat = async (data) => await httpClient.post('/chats/', data);

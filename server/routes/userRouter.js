403f180b1e7d84b22d106ae17977b7ab7b887e1b
Mykhailenko Mykhailo <mikhailenkomikhail1995@gmail.com>
2023-02-09 14:09:49 +0200

    route fixed

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -3,10 +3,10 @@ const {hashPass} = require('../middleware/hashPassword');
 const UserController = require('../controllers/user.controller');
 const { checkToken } = require('../middleware/checkToken');
 const userRouter = Router();
-
 userRouter.post('/sign-up', hashPass, UserController.signUpUser) // signUp
 userRouter.post('/sign-in', UserController.signInUser) // singIn
 userRouter.get('/', checkToken, UserController.getOneUser) // getOne user
+userRouter.post('/refresh', UserController.refreshSession); // refresh session
 
 
 module.exports = userRouter;
